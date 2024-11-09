9f5a101765eb8cbcc2da038854c67d1702e22f77
ezcg <mbbyrnes@gmail.com>
2024-11-09 08:44:11 -0800

    init

--- del_docker_images.sh
+++ del_docker_images.sh
@@ -17,8 +17,24 @@ do
     read -p "Delete [y/N]: " remove
     if [ "$remove" = "y" ]; then
       printf "\n\ndeleting $id"
-      docker rmi ${id}
+      r=$(docker rmi ${id})
+      echo "${r}"
       printf "\n\n"
+      if [[ $r == *"stopped container"* ]]; then
+        read -p "Delete the stopped container [y/N]: " delete
+        if [ "$delete" = "y" ]; then
+          cid=$(echo "$r" | grep -o "stopped container .*$")
+          printf "\n\n"
+          echo "Deleting stopped container ${cid}"
+          printf "\n\n"
+          docker rm "${cid}"
+          printf "\n\n"
+          echo "Trying to delete ${id} again"
+          printf "\n\n"
+          docker rmi "${id}"
+        fi
+      fi
+
     fi
   fi
 done
